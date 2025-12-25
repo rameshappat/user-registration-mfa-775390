af0271965f85c182408a3bf6f8fb59689f5df5ff
Ramesh Appat <rameshappat@gmail.com>
2025-12-25 16:03:01 -0700

    Add src/main/java/com/example/UserRegistrationRequest.java

---
+++ src/main/java/com/example/UserRegistrationRequest.java
@@ -0,0 +1,42 @@
+package com.example;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+public class UserRegistrationRequest {
+
+    @NotBlank
+    private String username;
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    // Getters and Setters
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
